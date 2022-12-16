d88e6c1b5da411eff6ebc5b43ef93c8e6c54194c
Ivan <saff@inbox.ru>
2022-12-16 16:13:29 +0300

    Homework 1

---
+++ ParserException.java
@@ -0,0 +1,15 @@
+// https://www.cyberforum.ru/java/thread926736.html :
+public class ParserException extends Exception{
+    
+    private static final long serialVersionUID = 1L;
+    private String errStr;  //  Описание ошибки
+ 
+    public ParserException(String errStr) {
+        super();
+        this.errStr = errStr;
+    }
+    
+    public String toString(){
+        return this.errStr;
+    }
+}
